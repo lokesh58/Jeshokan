520ddef365de1e7e86f3e64b86a21a09b9a957ee
Lokesh Patel <patellokesh05899@gmail.com>
2018-05-22 13:10:29 +0530

    Added PosKey generator

--- position.cpp
+++ position.cpp
@@ -1,9 +1,33 @@
 #include "position.h"
+#include "hash.h"
+#include "debug.h"
 
 Position::Position() {
-    //ctor
+    Hash::init();
 }
 
 Position::~Position() {
     //dtor
+}
+
+Key Position::__generatePosKey() const {
+    Key k = 0;
+
+    for (int sq = 0; sq < _numBoardSq; ++sq) {
+        int piece = board[sq];
+        if (piece != EMPTY) {
+            assert(0 <= piece && piece < EMPTY);
+            k ^= Hash::getHashKey(piece, sq);
+        }
+    }
+
+    assert(0 <= _enPassSq && _enPassSq < _numSquares);
+    k ^= Hash::getHashKey(EMPTY, sq);
+
+    assert(_side == WHITE || _side == BLACK);
+    if (_side == WHITE) {
+        k ^= Hash::getSideKey();
+    }
+
+    return k;
 }
--- position.h
+++ position.h
@@ -1,13 +1,20 @@
 #ifndef POSITION_H
 #define POSITION_H
 
+#include "types.h"
+
 class Position {
     public:
         Position();
         ~Position();
 
     private:
+        Key __generatePosKey() const;
 
+        int board[_numBoardSq];
+        int _side;
+        int _enPassSq;
+        Key _posKey;
 };
 
 #endif // POSITION_H
