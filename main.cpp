33e372daa92772ffc5da67c65507742d9e88c471
Lokesh Patel <patellokesh05899@gmail.com>
2018-06-12 09:52:24 +0530

    Added code to check parseFEN()

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
+#include "position.h"
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Position pos;
+    string fen = startFEN;
+    pos.parseFEN(fen);
+    pos.dispBoard();
+    fen = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1";
+    pos.parseFEN(fen);
+    pos.dispBoard();
+    fen = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2";
+    pos.parseFEN(fen);
+    pos.dispBoard();
+    fen = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2";
+    pos.parseFEN(fen);
+    pos.dispBoard();
     return 0;
 }
